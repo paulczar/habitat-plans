4a9e38ae535ff635eb1f598d10f230b04a2b6d8c
Paul Czarkowski <username.taken@gmail.com>
2016-07-10 19:48:54 +0000

    galera

--- galera/plan.sh
+++ galera/plan.sh
@@ -1,5 +1,3 @@
-set -xeo
-
 pkg_origin=paulczar
 pkg_name=galera
 pkg_version=3-25.3.16
@@ -7,71 +5,49 @@ pkg_maintainer="Paul Czarkowski <username.taken@gmail.com>"
 pkg_license=("Apache 2.0")
 pkg_source=http://releases.galeracluster.com/source/galera-${pkg_version}.tar.gz
 pkg_shasum=e123a7c5a054e377f94c6113eb62295bb7da82b5ab03cbf9505a0903ddbf995b
-# mysqld_safe
+
 pkg_deps=(
-#core/python
-#  core/coreutils
-#  core/gcc
-#  core/gcc-libs
-#  core/make
-#  core/bison
-#  core/zlib
-#  core/ncurses
-#  core/glibc
-#  core/cmake
-#  core/libaio
-#  core/libxml2
-#  core/pkg-config
-#  core/m4
-#  core/libbsd
-#  core/libedit
-#  core/texinfo
-#  core/bash
-#  core/openssl
-#  core/openssl-musl
-#  paulczar/jemalloc
+  core/gcc-libs
+  core/openssl
+  core/glibc
+  core/boost
 )
 
 pkg_build_deps=(
-#  paulczar/scons
+  paulczar/scons
   core/coreutils
   core/python2
-  core/cacerts
-  core/curl
-#  core/gcc
-#  core/gcc-libs
-#  core/make
-#  core/bison
-#  core/zlib
-#  core/ncurses
-#  core/glibc
-#  core/cmake
-#  core/libaio
-#  core/libxml2
-#  core/pkg-config
-#  core/m4
-#  core/libbsd
-#  core/libedit
-#  core/texinfo
-#  paulczar/jemalloc
+  core/gcc
+  core/gcc-libs
+  core/boost159
+  core/openssl
+  core/check
+  core/glibc
+  core/git
 )
-pkg_bin_dirs=(bin)
-pkg_include_dirs=(include)
-pkg_lib_dirs=(lib)
-pkg_expose=(3306)
 
+pkg_lib_dirs=(lib)
+pkg_bin_dirs=(bin)
+
+
+do_prepare() {
+    export CXXFLAGS="$CFLAGS"
+    export RUN_TESTS=0
+    export SCONS_OPTS="strict_build_flags=0 --config=force"
+    export EXTRA_SYSROOT="${pkg_prefix}"
+}
 
 do_build() {
-#    [[ -e /usr/bin/env ]] || ln -s /hab/pkgs/core/coreutils/8.24/20160612075329/bin/env /usr/bin/env
-  curl -k https://bootstrap.pypa.io/get-pip.py | python
-  pip install scons
-    cd galera
-    pwd
-    scons
+    scripts/build.sh 
 }
 
 do_install() {
-
-  echo 1
-#  make install
+    echo installing libgalera_smm.so to $pkg_prefix/lib
+    mkdir -p $pkg_prefix/lib
+    mkdir -p $pkg_prefix/bin
+    install -Dm0644 libgalera_smm.so $pkg_prefix/lib/libgalera_smm.so
+    install -Dm0755 garb/garbd $pkg_prefix/bin/garbd
+    install -Dm0644 scripts/packages/README $pkg_prefix/README
+    install -m0644 scripts/packages/README-MySQL $pkg_prefix/README-MySQL
+    install -m0644 COPYING $pkg_prefix/COPYING    
 }
